6efd753c5ba17eadae0160a9054b1104421f1f6c
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-05 12:14:19 -0700

    Final result

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate() {
     const suffix = this.dataset.sizing || '';
-    document.documentElement
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
     
 }
 
